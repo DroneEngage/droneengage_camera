8e1b97ac5dbd0937aca06fd22960b0e08345ef5f
HefnySco <mohammad.hefny@gmail.com>
2025-10-05 18:43:09 +0300

    version:3.6.1

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 6
-#define REVISION 0
+#define REVISION 1
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
 
