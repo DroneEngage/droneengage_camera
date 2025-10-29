f568c422b459acd527b5740fec9d50b4f6e69e15
HefnySco <mohammad.hefny@gmail.com>
2025-10-29 18:41:28 +0300

    moving config management to de_common

--- src/version.h
+++ src/version.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 8
-#define REVISION 3
+#define VERSION_MINOR 9
+#define REVISION 0
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
 
