930c6b5d53aedd054e0b7a939890f254c6127f49
HefnySco <mohammad.hefny@gmail.com>
2025-11-24 15:41:41 +0300

    sim camera

--- scripts/create_virtual_videos.sh
+++ scripts/create_virtual_videos.sh
@@ -2,7 +2,7 @@
 sudo apt install v4l2loopback-dkms
 sudo modprobe -r v4l2loopback
 sudo modprobe v4l2loopback devices=3
-sudo modprobe v4l2loopback devices=5 video_nr=1,2,3,4,5 card_label="DE-CAM1,DE-CAM2,DE-TRK,DE-RPI,DE-THERMAL" exclusive_caps=1,1,1,1,1
+sudo modprobe v4l2loopback devices=5 video_nr=1,2,3,4,5 card_label="DE-CAM1,SIM-CAM1,DE-TRK,DE-RPI,DE-THERMAL" exclusive_caps=1,1,1,1,1
 ls /sys/devices/virtual/video4linux/
 
 
