32928a9d929546a797718d66085184cdf0ae40c6
HefnySco <mohammad.hefny@gmail.com>
2026-02-08 20:11:23 +0300

    version:3.12.0

--- src/version.h
+++ src/version.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 11
-#define REVISION 2
+#define VERSION_MINOR 12
+#define REVISION 0
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
 
