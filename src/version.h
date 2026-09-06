8732cb48ea80e727015c7d6be076b11647abbcb7
HefnySco <mohammad.hefny@gmail.com>
2026-09-06 20:09:12 +0300

    version:5.10.2 chore: update de_common submodule (10.5.0)

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 10
-#define REVISION 0
+#define REVISION 2
 #define STRINGIFY(x) #x
 #define VERSION_STR(A,B,C) STRINGIFY(A) "." STRINGIFY(B) "."  STRINGIFY(C)
 
